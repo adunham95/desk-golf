705b4b87c7bb0b263e7e5e3efa029fc099a12f42
adunham95 <adunham95@gmail.com>
2021-08-09 13:06:18 -0400

    Added sign in page

---
+++ pages/signin.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import { Layout } from '../components/layout';
+
+const SignInPage = () => {
+  const [] = useState('');
+  return (
+    <Layout pageTitle="Login">
+      <div>
+        <div className="min-h-50 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-md w-full space-y-8">
+            <div>
+              <img
+                className="mx-auto h-12 w-auto"
+                src="/img/icon.png"
+                alt="Workflow"
+              />
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Sign in to your account
+              </h2>
+              <p className="mt-2 text-center text-sm text-gray-600">
+                Or
+                <a
+                  href="#"
+                  className="font-medium text-primary hover:text-primaryLight"
+                >
+                  <span> </span>
+                  start your 14-day free trial
+                </a>
+              </p>
+            </div>
+            <form
+              className="mt-8 space-y-6"
+              action="#"
+              method="POST"
+            >
+              <input
+                type="hidden"
+                name="remember"
+                value="true"
+              />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label
+                    htmlFor="email-address"
+                    className="sr-only"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                    placeholder="Email address"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="sr-only"
+                  >
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-primary focus:ring-primary border-gray-300 rounded"
+                  />
+                  <label
+                    htmlFor="remember-me"
+                    className="ml-2 block text-sm text-gray-900"
+                  >
+                    Remember me
+                  </label>
+                </div>
+
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-medium text-primary hover:text-primaryDark"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-primaryDark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primaryLight"
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                    {/* <!-- Heroicon name: solid/lock-closed --> */}
+                    <svg
+                      className="h-5 w-5 text-primaryDark group-hover:text-primaryLight"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default SignInPage;
