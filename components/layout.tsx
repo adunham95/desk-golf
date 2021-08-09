d85bdcfad23abbd4419cf3256224b542d0f36eee
adunham95 <adunham95@gmail.com>
2021-08-09 11:28:52 -0400

    Added favicons

--- components/layout.tsx
+++ components/layout.tsx
@@ -34,6 +34,23 @@ export const Layout = ({
         rel="icon"
         href="/favicon.ico"
       />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
     </Head>
     {banner.show && (
     <Banner
