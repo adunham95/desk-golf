ac2f4a2c43070f282f68e23cde078dcab5d0b168
adunham95 <adunham95@gmail.com>
2021-08-09 11:40:42 -0400

    Added theme

--- components/banner.tsx
+++ components/banner.tsx
@@ -5,13 +5,13 @@ interface BannerInterface{
   }
 
 export const Banner = ({ text, link, cta }: BannerInterface) => (
-  <div className="bg-indigo-600">
+  <div className="bg-primary">
     <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between flex-wrap">
         <div className="w-0 flex-1 flex items-center">
-          <span className="flex p-2 rounded-lg bg-indigo-800">
+          <span className="flex p-2 rounded-lg bg-primaryDark">
             <svg
-              className="h-6 w-6 text-white"
+              className="h-6 w-6 text-black"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -26,7 +26,7 @@ export const Banner = ({ text, link, cta }: BannerInterface) => (
               />
             </svg>
           </span>
-          <p className="ml-3 font-medium text-white truncate">
+          <p className="ml-3 font-medium text-black truncate">
             <span className="md:hidden">
               {text}
             </span>
@@ -38,7 +38,7 @@ export const Banner = ({ text, link, cta }: BannerInterface) => (
         <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
           <a
             href="#"
-            className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+            className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-primaryDark bg-white hover:bg-primaryExtraLight"
           >
             {cta}
           </a>
@@ -46,11 +46,11 @@ export const Banner = ({ text, link, cta }: BannerInterface) => (
         <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
           <button
             type="button"
-            className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+            className="-mr-1 flex p-2 rounded-md hover:bg-primaryDark focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
           >
             <span className="sr-only">Dismiss</span>
             <svg
-              className="h-6 w-6 text-white"
+              className="h-6 w-6 text-black"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const themeColors = {
+  primary: '#95e1d3',
+  primaryLight: '#bdece3',
+  primaryDark: '#6dd6c2',
+  primaryText: '#e6e6e6',
+  primaryExtraLight: '#e4f7f4',
+};
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: themeColors,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
