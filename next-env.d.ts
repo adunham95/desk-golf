16adba7398c457873fa76fab18ba5d56a5e490ac
adunham95 <adunham95@gmail.com>
2021-08-09 20:42:41 -0400

    Added graphql stuff

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+
+declare module '*.graphqls' {
+    import { DocumentNode } from 'graphql';
+
+    export default typeof DocumentNode;
+  }
+
+  declare module '*.yml'
