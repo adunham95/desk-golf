7db27c945d0161ed6401671e67954727f308d4be
adunham95 <adunham95@gmail.com>
2021-08-10 06:53:47 -0400

    Removed GraphQL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,10 @@ import '../styles/globals.css';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
-import { ApolloProvider } from '@apollo/client';
-import { useApollo } from '../lib/apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps.initialApolloState);
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <Component {...pageProps} />
   );
 }
 export default MyApp;
