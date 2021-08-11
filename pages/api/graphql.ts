3e33515dec0345590070f3833862d5f6131be471
adunham95 <adunham95@gmail.com>
2021-08-11 08:20:03 -0400

    Moved connections to apollo server

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,28 @@
 import { ApolloServer } from 'apollo-server-micro';
+import { client } from '../../db/db';
 import { schema } from '../../api/schema';
 
-const apolloServer = new ApolloServer({ schema });
+let db;
+
+const apolloServer = new ApolloServer({
+  schema,
+  context: async () => {
+    if (!db) {
+      try {
+        // @ts-ignore
+        // if (!client.isConnected()) await client.connect();
+        await client.connect();
+        db = client.db('admin'); // database name
+
+        console.log('🗄  Connected to DB success');
+      } catch (e) {
+        console.log('--->error while connecting with graphql context (db)', e);
+      }
+    }
+
+    return { db };
+  },
+});
 
 export const config = {
   api: {
